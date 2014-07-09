31aa2c8d4e0f03208b98fa3be030ffd0ca804b73
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-07-09 17:21:58 -0600

    Add exercism-analysis dependency. Render analysis results in view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+  end
+
+  def analyze
+    @analysis_results = Exercism::Analysis.new(Exercism::Adapters::Ruby.new(params[:code])).run(Exercism::Analyzers::IterMutation)
+  end
+
 end
